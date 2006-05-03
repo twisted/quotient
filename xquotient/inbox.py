ad7777eec0d02fe92e362c3c9bec66c7cf384b6a
exarkun <>
2006-05-03 18:42:52 +0000

    Merge nev-order-928
    Author: exarkun
    Reviewer: moe
    Fixes #928
    
    Remove the priority averaging code from the nav system.  Force top-level
    priority to be exactly what the authoritative Tab says it should be.
    
    Change Sine and Quotient priorities so that Quotient comes first by
    specifying a higher priority and Sine comes last by specifying a lower
    priority.

--- xquotient/inbox.py
+++ xquotient/inbox.py
@@ -91,7 +91,7 @@ class Inbox(Item, InstallableMixin):
     # the user changes the setting)
 
     def getTabs(self):
-        return [webnav.Tab('Mail', self.storeID, 0.25, children=
+        return [webnav.Tab('Mail', self.storeID, 0.75, children=
                     [webnav.Tab('Inbox', self.storeID, 0.4)],
                 authoritative=True)]
 
