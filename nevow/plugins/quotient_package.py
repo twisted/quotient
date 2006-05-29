57e1cd2fa82a920cd64521e48a93d89bf14125a1
mithrandi <>
2006-05-29 16:34:24 +0000

    Merge js-modules-651.
    Fixes #651
    Author: mithrandi
    Reviewer: exarkun
    
    This branch implements {{{athena.AutoJSPackage}}} for use in nevow plugins; it
    automatically derives JS module mappings from the filesystem hierarchy, in a
    manner similar to Python. Unlike python, __init__.js files are optional, and
    will be treated like an empty file if not present. Mantissa and Quotient are
    also migrated to use the new mechanism.

--- nevow/plugins/quotient_package.py
+++ nevow/plugins/quotient_package.py
@@ -2,19 +2,4 @@ from twisted.python import filepath
 from nevow import athena
 
 import xquotient
-
-jsdir = filepath.FilePath(xquotient.__file__).parent().child('static').child('js')
-
-quotient = athena.JSPackage({
-    u'Quotient': jsdir.child('quotient.js').path,
-    u'Quotient.Common':  jsdir.child('common.js').path,
-    u'Quotient.Mailbox': jsdir.child('mailbox.js').path,
-    u'Quotient.Compose': jsdir.child('compose.js').path,
-    u'Quotient.Gallery': jsdir.child('gallery.js').path,
-    u'Quotient.Grabber': jsdir.child('grabber.js').path,
-    u'Quotient.Filter': jsdir.child('filter.js').path,
-    u'Quotient.Extracts': jsdir.child('extracts.js').path,
-    u'Quotient.Test': filepath.FilePath(xquotient.__file__).parent().child('test').child('livetest.js').path,
-
-    u'LightBox': jsdir.child('lightbox.js').path,
-})
+quotient = athena.AutoJSPackage(filepath.FilePath(xquotient.__file__).parent().child('js').path)
