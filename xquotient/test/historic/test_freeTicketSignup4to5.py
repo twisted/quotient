805fd6d1ba2a0c944fc580a708ab0a5487866ded
moe <>
2006-06-09 21:45:35 +0000

    this shouldn't be here

--- xquotient/test/historic/test_freeTicketSignup4to5.py
+++
@@ -1,6 +0,0 @@
-from axiom.test.historic import stubloader
-from xmantissa.signup import PasswordReset
-
-class FreeTicketSignupTestCase(stubloader.StubbedTest):
-    def testUpgrade(self):
-        self.store.findUnique(PasswordReset)
