24f908b33ecf98e519f97ebd582f4aca6afbef29
exarkun <>
2006-08-17 16:56:07 +0000

    Merge pop3-ssl-1336
    Author: exarkun
    Reviewer: glyph
    Fixes #1336
    
    This adds support for configuring the POP3/SSL server in Quotient via
    an axiomatic plugin.  It also renames the existing "mail" command to
    "smtp" to disambiguate it from POP3.

--- axiom/plugins/mailcmd.py
+++ axiom/plugins/mailcmd.py
@@ -6,24 +6,22 @@ $ axiomatic mail
 
 from axiom.plugins.webcmd import decodeCommandLine
 from axiom.scripts.axiomatic import AxiomaticCommand
+from axiom.errors import ItemNotFound
 
 from twisted.python.usage import UsageError
 
 from xquotient.mail import MailTransferAgent
+from xquotient.popout import POP3Listener
 
-class SMTPConfiguration(AxiomaticCommand):
-    """
-    Subcommand for configuring and introspecting an MTA installed on an Axiom
-    database.
-    """
-    name = 'mail'
-    description = 'SMTP configuration and introspection'
 
+class _PortConfigurationMixin:
     optParameters = [
         ('port', 'p', None,
-         'TCP port over which to serve SMTP (empty string to disable)'),
+         'TCP port over which to serve plaintext connections '
+         '(empty string to disable)'),
         ('secure-port', 's', None,
-         'TCP port over which to server SMTP/SSL (empty string to disable)'),
+         'TCP port over which to server SSL connections '
+         '(empty string to disable)'),
         ('pem-file', 'f', None,
          'Filename containing PEM-format private key and certificate '
          '(empty string to disable; ignored if --secure-port is not '
@@ -31,7 +29,7 @@ class SMTPConfiguration(AxiomaticCommand):
         ]
 
 
-    def _setPortNumber(self, mta, key='port', attr='portNumber'):
+    def _setPortNumber(self, server, key='port', attr='portNumber'):
         if self[key] is not None:
             if not self[key]:
                 port = None
@@ -44,22 +42,22 @@ class SMTPConfiguration(AxiomaticCommand):
                     raise UsageError(
                         "Argument to --" + key + " must be an "
                         "integer between 0 and 65536.")
-            setattr(mta, attr, port)
+            setattr(server, attr, port)
             return True
         return False
 
 
-    def _setSecurePortNumber(self, mta):
-        return self._setPortNumber(mta, 'secure-port', 'securePortNumber')
+    def _setSecurePortNumber(self, server):
+        return self._setPortNumber(server, 'secure-port', 'securePortNumber')
 
 
-    def _setCertificate(self, mta):
+    def _setCertificate(self, server):
         if self['pem-file'] is not None:
             if not self['pem-file']:
                 certificateFile = None
             else:
                 certificateFile = self['pem-file']
-            mta.certificateFile = certificateFile
+            server.certificateFile = certificateFile
             return True
         return False
 
@@ -69,16 +67,38 @@ class SMTPConfiguration(AxiomaticCommand):
         def reconfigure():
             didSomething = False
             try:
-                mta = store.findUnique(MailTransferAgent)
+                server = store.findUnique(self.serverType)
             except ItemNotFound:
-                raise UsageError("No MTA installed on this store.")
+                raise UsageError("No %s server installed on this store." % (self.name,))
             for f in [self._setPortNumber,
                       self._setSecurePortNumber,
                       self._setCertificate]:
-                didSomething += f(mta)
+                didSomething += f(server)
             return didSomething
         if not store.transact(reconfigure):
             self.opt_help()
+
+
+
+class POPConfiguration(_PortConfigurationMixin, AxiomaticCommand):
+    """
+    Subcommand for configuring and introspecting a POP3 server installed on an
+    Axiom database.
+    """
+    name = 'pop3'
+    description = 'POP3 configuration and introspection'
+    serverType = POP3Listener
+
+
+
+class SMTPConfiguration(_PortConfigurationMixin, AxiomaticCommand):
+    """
+    Subcommand for configuring and introspecting an MTA installed on an Axiom
+    database.
+    """
+    name = 'smtp'
+    description = 'SMTP configuration and introspection'
+    serverType = MailTransferAgent
 
 
 del AxiomaticCommand
--- xquotient/test/test_mailcmd.py
+++ xquotient/test/test_mailcmd.py
@@ -3,16 +3,26 @@ from twisted.trial.unittest import TestCase
 from twisted.python.usage import UsageError
 
 from axiom.store import Store
-from axiom.plugins.mailcmd import SMTPConfiguration
+from axiom.plugins.mailcmd import SMTPConfiguration, POPConfiguration
 from axiom.test.util import CommandStubMixin
 
 from xquotient.mail import MailTransferAgent
+from xquotient.popout import POP3Listener
 
-class ConfigurationTestCase(CommandStubMixin, TestCase):
+class ConfigurationMixin(CommandStubMixin):
     def setUp(self):
         self.store = Store()
-        self.mta = MailTransferAgent(store=self.store)
-        self.mta.installOn(self.store)
+        self.server = self.createServer(self.store)
+        self.server.installOn(self.store)
+        self.config = self.createOptions()
+
+
+    def createServer(self):
+        raise NotImplementedError()
+
+
+    def createOptions(self):
+        raise NotImplementedError()
 
 
     def test_shortOptionParsing(self):
@@ -20,7 +30,7 @@ class ConfigurationTestCase(CommandStubMixin, TestCase):
         Test that the short forms of all supported command line options are
         parsed correctly.
         """
-        opt = SMTPConfiguration()
+        opt = self.config
         opt.parent = self
         opt.parseOptions([
                 '-p', '8025', '-s', '8465',
@@ -35,7 +45,7 @@ class ConfigurationTestCase(CommandStubMixin, TestCase):
         Test that the long forms of all supported command line options are
         parsed correctly.
         """
-        opt = SMTPConfiguration()
+        opt = self.config
         opt.parent = self
         opt.parseOptions([
                 '--port', '8025', '--secure-port', '8465',
@@ -50,11 +60,11 @@ class ConfigurationTestCase(CommandStubMixin, TestCase):
         Test that passing a new port number changes the port number of the
         MailTransferAgent on the store.
         """
-        opt = SMTPConfiguration()
+        opt = self.config
         opt.parent = self
         opt['port'] = '12345'
         opt.postOptions()
-        self.assertEquals(self.mta.portNumber, 12345)
+        self.assertEquals(self.server.portNumber, 12345)
 
 
     def test_unsetPortNumber(self):
@@ -62,11 +72,11 @@ class ConfigurationTestCase(CommandStubMixin, TestCase):
         Test that passing an empty string for the port number changes the port
         number of the MailTransferAgent to None.
         """
-        opt = SMTPConfiguration()
+        opt = self.config
         opt.parent = self
         opt['port'] = ''
         opt.postOptions()
-        self.assertEquals(self.mta.portNumber, None)
+        self.assertEquals(self.server.portNumber, None)
 
 
     def test_outOfBoundsPortNumber(self):
@@ -74,7 +84,7 @@ class ConfigurationTestCase(CommandStubMixin, TestCase):
         Test that specifying a port number out of the allowed range results in
         a UsageError being raised.
         """
-        opt = SMTPConfiguration()
+        opt = self.config
         opt.parent = self
         badValues = ['-1', '0', '65536', '65537']
         for v in badValues:
@@ -87,11 +97,11 @@ class ConfigurationTestCase(CommandStubMixin, TestCase):
         Test that passing a new ssl port number changes the secure port number
         of the MailTransferAgent on the store.
         """
-        opt = SMTPConfiguration()
+        opt = self.config
         opt.parent = self
         opt['secure-port'] = '54321'
         opt.postOptions()
-        self.assertEquals(self.mta.securePortNumber, 54321)
+        self.assertEquals(self.server.securePortNumber, 54321)
 
 
     def test_unsetSecurePortNumber(self):
@@ -99,11 +109,11 @@ class ConfigurationTestCase(CommandStubMixin, TestCase):
         Test that passing an empty string for the secure port number changes
         the secure port number of the MailTransferAgent to None.
         """
-        opt = SMTPConfiguration()
+        opt = self.config
         opt.parent = self
         opt['secure-port'] = ''
         opt.postOptions()
-        self.assertEquals(self.mta.securePortNumber, None)
+        self.assertEquals(self.server.securePortNumber, None)
 
 
     def test_outOfBoundsPortNumber(self):
@@ -111,7 +121,7 @@ class ConfigurationTestCase(CommandStubMixin, TestCase):
         Test that specifying a secure port number out of the allowed range
         results in a UsageError being raised.
         """
-        opt = SMTPConfiguration()
+        opt = self.config
         opt.parent = self
         badValues = ['-1', '0', '65536', '65537']
         for v in badValues:
@@ -124,8 +134,28 @@ class ConfigurationTestCase(CommandStubMixin, TestCase):
         Test that specifying a pem file causes the certificateFile attribute of
         the MailTransferAgent to be set.
         """
-        opt = SMTPConfiguration()
+        opt = self.config
         opt.parent = self
         opt['pem-file'] = 'server.pem'
         opt.postOptions()
-        self.assertEquals(self.mta.certificateFile, 'server.pem')
+        self.assertEquals(self.server.certificateFile, 'server.pem')
+
+
+
+class SMTPConfigurationTestCase(ConfigurationMixin, TestCase):
+    def createServer(self, store):
+        return MailTransferAgent(store=self.store)
+
+
+    def createOptions(self):
+        return SMTPConfiguration()
+
+
+
+class POP3ConfigurationTestCase(ConfigurationMixin, TestCase):
+    def createServer(self, store):
+        return POP3Listener(store=self.store)
+
+
+    def createOptions(self):
+        return POPConfiguration()
