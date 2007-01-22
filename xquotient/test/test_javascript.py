7dd0400155216a47de531165d2968251303e81d1
exarkun <>
2007-01-22 21:45:04 +0000

    Merge placeholder-testcase-1916
    Author: exarkun
    Reviewer: moe (except the Quotient test_javascript.py change)
    Fixes #1916
    
    Update the JavaScript Placeholder model unit tests to use the new mechanism
    provided by Nevow which yields improved trial integration and puts more of the
    harness code into the framework.

--- xquotient/test/test_javascript.py
+++ xquotient/test/test_javascript.py
@@ -5,8 +5,83 @@
 Runs Quotient javascript tests as part of the Quotient python test suite
 """
 
+import sys
+
 from twisted.python.filepath import FilePath
-from nevow.testutil import JavaScriptTestSuite, setJavascriptInterpreterOrSkip
+from twisted.python.util import untilConcludes
+from twisted.trial.unittest import TestCase
+from twisted.internet.protocol import ProcessProtocol
+from twisted.internet.error import ProcessTerminated
+from twisted.internet.defer import Deferred
+from twisted.internet import reactor
+
+from nevow.testutil import setJavascriptInterpreterOrSkip
+from nevow.scripts import consolejstest
+
+# XXX TODO: Rewrite all of this use Divmod.UnitTest instead.
+class _JavaScriptTestSuiteProtocol(ProcessProtocol):
+    finished = None
+
+    def connectionMade(self):
+        self.out = []
+        self.err = []
+
+    def outReceived(self, out):
+        untilConcludes(sys.stdout.write, out)
+        untilConcludes(sys.stdout.flush)
+        self.out.append(out)
+
+    def errReceived(self, err):
+        untilConcludes(sys.stdout.write, err)
+        untilConcludes(sys.stdout.flush)
+        self.err.append(err)
+
+    def processEnded(self, reason):
+        if reason.check(ProcessTerminated):
+            self.finished.errback(Exception(reason.getErrorMessage(), ''.join(self.out), ''.join(self.err)))
+        elif self.err:
+            self.finished.errback(Exception(''.join(self.out), ''.join(self.err)))
+        else:
+            self.finished.callback(''.join(self.out))
+
+
+
+class JavaScriptTestSuite(TestCase):
+    """
+    Inherit from me if you want to run javascript tests
+
+    @ivar path: path to directory containing the javascipt files
+    @type path: L{twisted.python.filepath.FilePath}
+    """
+    javascriptInterpreter = None
+    path = None
+
+    def onetest(self, jsfile):
+        """
+        Test the javascript file C{jsfile}
+
+        @param jsfile: filename
+        @type jsfile: C{str}
+
+        @return: deferred that fires when the javascript interpreter process
+        terminates
+        @rtype: L{twisted.interner.defer.Deferred}
+        """
+        p = _JavaScriptTestSuiteProtocol()
+        d = p.finished = Deferred()
+
+        fname = self.mktemp()
+        file(fname, 'w').write(
+            consolejstest.generateTestScript(self.path.child(jsfile).path))
+
+        reactor.spawnProcess(
+            p,
+            self.javascriptInterpreter,
+            ("js", fname))
+
+        return d
+
+
 
 class QuotientJavaScriptTestSuite(JavaScriptTestSuite):
     """
