f9e2ff7bcfdfb5093521871b1943c3b0168625a4
exarkun <>
2007-05-21 11:19:08 +0000

    Merge cleanup-liveform-2094
    Author: exarkun
    Reviewer: glyph
    Fixes #2094
    
    Factor the some of the view generation code for LiveForm out of the monolithic
    form renderer and into parameter-specific element classes.  Also allow the view
    for each parameter to be specified along with the parameter itself to override
    the default.
    
    Also move Quotient's rendertools module into Mantissa to use it for some
    unit tests there.

--- xquotient/benchmarks/rendertools.py
+++ xquotient/benchmarks/rendertools.py
@@ -1,49 +1,9 @@
 # -*- test-case-name: xquotient.test.test_rendertools -*-
 
 """
-Simpler Nevow-related rendering helpers for Quotient benchmarks.
+Deprecated backwards-compatibility module.
 """
 
-from nevow.rend import Page
-from nevow.athena import LivePage
-from nevow.loaders import stan
-from nevow.testutil import FakeRequest
-from nevow.context import WovenContext
-from nevow.inevow import IRequest
+from xmantissa.test.rendertools import (
+    renderLiveFragment as render, renderPlainFragment)
 
-
-def _makeContext():
-    request = FakeRequest()
-    context = WovenContext()
-    context.remember(request, IRequest)
-    return (request, context)
-
-def render(fragment):
-    """
-    Render the given fragment in a LivePage.
-
-    This can only work for fragments which can be rendered synchronously.
-    Fragments which involve Deferreds will be silently rendered incompletely.
-
-    @type fragment: L{nevow.athena.LiveFragment} or L{nevow.athena.LiveElement}
-    @param fragment: The page component to render.
-
-    @rtype: C{str}
-    @return: The result of rendering the fragment.
-    """
-    page = LivePage(docFactory=stan(fragment))
-    fragment.setFragmentParent(page)
-    (request, context) = _makeContext()
-    page.renderHTTP(context)
-    page.action_close(context)
-    return request.v
-
-def renderPlainFragment(fragment):
-    """
-    same as L{render}, but expects an L{nevow.rend.Fragment} or any
-    other L{nevow.inevow.IRenderer}
-    """
-    page = Page(docFactory=stan(fragment))
-    (request, context) = _makeContext()
-    page.renderHTTP(context)
-    return request.v
