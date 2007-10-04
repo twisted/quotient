a703067078556381516c38e805331504b319d111
pjd <>
2007-10-04 15:07:56 +0000

    Observation support

--- xquotient/console.py
+++ xquotient/console.py
@@ -2,7 +2,7 @@ from zope.interface import implements
 from twisted.python.components import registerAdapter
 
 from nevow import loaders, tags, athena
-from axiom import item, dependency
+from axiom import item, dependency, attributes
 from xmantissa import ixmantissa, webnav, people
 
 class Console(item.Item):
@@ -14,6 +14,20 @@ class Console(item.Item):
     powerupInterfaces = (ixmantissa.INavigableElement,)
 
     organizer = dependency.dependsOn(people.Organizer)
+
+    _observers = attributes.inmemory()
+
+    def activate(self):
+        self._observers = set()
+
+
+    def addObserver(self, observer):
+        self._observers.add(observer)
+
+
+    def removeObserver(self, observer):
+        self._observers.remove(observer)
+
 
     # INavigableElement
     def getTabs(self):
@@ -27,6 +41,13 @@ class LiveLog(athena.LiveElement):
 
     def __init__(self, console):
         self.console = console
+
+
+    @athena.expose
+    def clientLoaded(self):
+        self.console.addObserver(self)
+        self.page.notifyOnDisconnect().addBoth(
+            lambda _: self.console.removeObserver(self))
 
 
     # INavigableFragment
--- xquotient/js/Quotient/Console.js
+++ xquotient/js/Quotient/Console.js
@@ -3,3 +3,7 @@
 // import Nevow.Athena
 
 Quotient.Console.LiveLog = Nevow.Athena.Widget.subclass('Quotient.Console.LiveLog');
+Quotient.Console.LiveLog.methods(
+    function loaded(self) {
+        self.callRemote('clientLoaded');
+    });
