31a20bd222906d087655a6350be3981c627d7ece
pjd <>
2007-10-04 15:21:46 +0000

    Entry formatting

--- xquotient/js/Quotient/Console.js
+++ xquotient/js/Quotient/Console.js
@@ -6,4 +6,27 @@ Quotient.Console.LiveLog = Nevow.Athena.Widget.subclass('Quotient.Console.LiveLo
 Quotient.Console.LiveLog.methods(
     function loaded(self) {
         self.callRemote('clientLoaded');
+    },
+
+    function newEntry(self, entry) {
+        function makeText() {
+            var text;
+            if (entry.href) {
+                text = document.createElement('a');
+                text.setAttribute('href', entry.text);
+                text.appendChild(document.createTextNode(entry.text));
+            } else {
+                text = document.createTextNode(entry.text);
+            }
+            var bold = document.createElement('b');
+            bold.appendChild(text);
+            return bold;
+        }
+        var e = document.createElement('div');
+        e.setAttribute('style', 'margin: 1em');
+        e.appendChild(document.createTextNode('Found '+entry.type+': '));
+        e.appendChild(document.createTextNode('...'+entry.before));
+        e.appendChild(makeText());
+        e.appendChild(document.createTextNode(entry.after+'...'));
+        self.node.appendChild(e);
     });
